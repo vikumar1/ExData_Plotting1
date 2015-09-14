d1adf4db696dd724c9db46bae9f1c5de62ca75d9
Vijay Kumar <vijaybihari1@gmail.com>
2015-09-14 23:55:50 +0530

    creating plot1.R

---
+++ plot1.R-source-Load_data.R-plot1---paste-getwd/graficos/plot1.png-sep-if-file.exists-plot1-png-graficos/plot1.R
@@ -0,0 +1,13 @@
+
+
+source("Load_data.R")
+
+plot1 <- paste(getwd(), "/graficos/plot1.png", sep = "")
+
+if(!file.exists(plot1)){
+        png("graficos/plot1.png", width = 480, height = 480)
+        hist(cargardatos$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+        dev.off()
+} else {
+        hist(cargardatos$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+}
